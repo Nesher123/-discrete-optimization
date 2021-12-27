022ac50420392cd25d3f4e00ef023c6af6367bb9
ofirnesher-nemodata <Nesher123@gmail.com>
2021-12-27 18:43:48 +0200

    implemented a DP approach

--- assignment 2 (knapsack)/solver.py
+++ assignment 2 (knapsack)/solver.py
@@ -41,6 +41,40 @@ def greedy_with_densities(items: list, capacity: int) -> [float, float]:
     return add_items_from_list_by_order_until_full(sorted_items_by_density, capacity)
 
 
+def dp(items: list, capacity: int) -> [float, float]:
+    """a Dynamic Programming approach for filling the knapsack"""
+    n = len(items)
+    table = [[0] * (n + 1) for _ in range(capacity + 1)]
+    taken = [0] * len(items)
+
+    for j in range(1, n + 1):
+        item = items[j - 1]
+
+        for i in range(min([i.weight for i in items]), capacity + 1):
+            if item.weight <= i:
+                table[i][j] = max(
+                    table[i][j - 1],  # not selected
+                    item.value + table[i - item.weight][j - 1]  # selected
+                )
+            else:
+                table[i][j] = table[i][j - 1]
+
+    value = table[-1][-1]
+
+    # trace back
+    j = n
+    i = capacity
+
+    while (j > 0) & (i > 0):
+        if table[i][j] > table[i][j - 1]:
+            taken[j - 1] = 1
+            i = i - [x.weight for x in items if x.index == j - 1][0]
+
+        j = j - 1
+
+    return value, taken
+
+
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
 
@@ -59,7 +93,8 @@ def solve_it(input_data):
         items.append(Item(i - 1, int(parts[0]), int(parts[1])))
 
     # value, taken = naive(items, capacity)  # naive approach
-    value, taken = greedy_with_densities(items, capacity)  # greedy approach
+    # value, taken = greedy_with_densities(items, capacity)  # greedy approach
+    value, taken = dp(items, capacity)  # dynamic programming approach
 
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(0) + '\n'
@@ -77,3 +112,13 @@ if __name__ == '__main__':
     else:
         print(
             'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
+
+    # file_location = './data/ks_4_0'.strip()
+    #
+    # with open(file_location, 'r') as input_data_file:
+    #     input_data = input_data_file.read()
+    #
+    # print(solve_it(input_data))
+
+# python ./solver.py ./data/ks_4_0
+# python ./submit.py
