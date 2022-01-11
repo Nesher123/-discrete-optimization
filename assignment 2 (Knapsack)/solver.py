8ec9e7ffef3f0d7345662929a13ee3b4659d2ecc
OfirNesher <ofir.nesher@nemodata.ai>
2022-01-11 10:40:12 +0200

    remove comments from assignment 2 solver

--- assignment 2 (Knapsack)/solver.py
+++ assignment 2 (Knapsack)/solver.py
@@ -115,13 +115,3 @@ if __name__ == '__main__':
     else:
         print(
             'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
-
-    # file_location = './data/ks_4_0'.strip()
-    #
-    # with open(file_location, 'r') as input_data_file:
-    #     input_data = input_data_file.read()
-    #
-    # print(solve_it(input_data))
-
-# python ./solver.py ./data/ks_4_0
-# python ./submit.py
