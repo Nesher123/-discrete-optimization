c4412a0dc57f3cd335c4c4319663b7b9c4e25258
OfirNesher <ofir.nesher@nemodata.ai>
2022-01-12 14:36:17 +0200

    a better trivial

--- assignment 3 (graph coloring)/solver.py
+++ assignment 3 (graph coloring)/solver.py
@@ -1,6 +1,49 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Helper functions
+# def remove_duplicates(lst):
+#     return list({*map(tuple, map(sorted, lst))})
+
+
+# Different solutions functions
+def trivial(node_count: int) -> range:
+    """
+    build a trivial solution
+    every node has its own color
+    """
+    return range(0, node_count)
+
+
+def find_indices(lst, condition):
+    return [i for i, elem in enumerate(lst) if condition(elem)]
+
+
+def greedy(node_count: int, edges: list[tuple[int, int]]) -> list[int]:
+    """
+    greedy approach:
+    all vertices are given color i.
+    then, loop on the vertices, and assign color i+1 to every adjacent node
+    """
+    solution = [0] * node_count
+
+    for i, j in edges:
+        if solution[j] == solution[i]:
+            solution[j] = solution[i] + 1
+
+    for i in range(0, node_count):
+        adjacent_vertices = find_indices(solution, lambda e: e == i)
+        import itertools
+
+        a = [adjacent_vertices] * 2
+        edges_inner = tuple(itertools.product(*a))
+
+        for e in edges_inner:
+            if e in edges:
+                solution[e[1]] = solution[e[1]] + 1
+
+    return solution
+
 
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
@@ -19,12 +62,12 @@ def solve_it(input_data):
         parts = line.split()
         edges.append((int(parts[0]), int(parts[1])))
 
-    # build a trivial solution
-    # every node has its own color
-    solution = range(0, node_count)
+    # solution = trivial(node_count)
+    solution = greedy(node_count, edges)
+    objective_value = len(set(solution))
 
     # prepare the solution in the specified output format
-    output_data = str(node_count) + ' ' + str(0) + '\n'
+    output_data = str(objective_value) + ' ' + str(0) + '\n'
     output_data += ' '.join(map(str, solution))
 
     return output_data
@@ -41,5 +84,5 @@ if __name__ == '__main__':
 
         print(solve_it(input_data))
     else:
-        print(
-            'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)')
+        print('This test requires an input file.  Please select one from the data directory. '
+              '(i.e. python solver.py ./data/gc_4_1)')
