7d8100c85f642a46097cf813d9d2521d78bb03ad
OfirNesher <ofir.nesher@nemodata.ai>
2022-01-23 15:54:00 +0200

    improved results using Simulated Annealing

--- assignment 4 (Traveling Salesman)/solver.py
+++ assignment 4 (Traveling Salesman)/solver.py
@@ -4,14 +4,12 @@
 import math
 from collections import namedtuple
 import sys
-import matplotlib.pyplot as plt
 import logging
-import json5 as json
-from datetime import timedelta
-from minizinc import Instance, Model, Solver, Status, MiniZincError
 import numpy as np
+import json5 as json
+import random
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 Point = namedtuple('Point', ['x', 'y', 'i'])
 
 """
@@ -19,31 +17,18 @@ Helper functions
 """
 
 
-def plot_points(points: list[Point]) -> None:
-    x = [p[0] for p in points]  # x-axis values
-    y = [p[1] for p in points]  # y-axis values
-
-    plt.figure()
-
-    for p in points:
-        plt.annotate(p.i, (p.x, p.y))
-
-    plt.scatter(x, y)  # plotting points as a scatter plot
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('plot')
-    plt.show()
-
-
-def get_ordered_list(points: list[Point], x, y) -> list[Point]:
-    # points = [Point(x=0.0, y=0.0, i=0), Point(x=1.0, y=1.0, i=1), Point(x=0.0, y=0.5, i=2)]
-    points.sort(key=lambda p: (p.x - x) ** 2 + (p.y - y) ** 2)
-
-    return points
-
-
 def length(point1, point2):
     return math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
+
+
+def calculate_length_of_tour(points: list[Point], solution: list[int]) -> float:
+    """calculate the length of the tour"""
+    objective = length(points[solution[-1]], points[solution[0]])
+
+    for index in range(0, len(solution) - 1):
+        objective += length(points[solution[index]], points[solution[index + 1]])
+
+    return objective
 
 
 """
@@ -51,12 +36,12 @@ Different solutions functions
 """
 
 
-def trivial(node_count: int) -> range:
+def trivial(node_count: int) -> list[int]:
     """
     build a trivial solution
     visit the nodes in the order they appear in the file
     """
-    return range(0, node_count)
+    return list(range(0, node_count))
 
 
 def greedy(points: list[Point]) -> list[int]:
@@ -67,18 +52,50 @@ def greedy(points: list[Point]) -> list[int]:
     solution = [0]
     original_length = len(points)
     j = 0
-    next = points[0]
+    next_vertex = points[0]
     points.pop(0)
 
     while j < original_length - 1:
-        x, y, i = next
+        x, y, i = next_vertex
         closest = np.argmin([(p.x - x) ** 2 + (p.y - y) ** 2 for p in points if p.i != i])
-        next = points[closest]
-        solution.append(next.i)
+        next_vertex = points[closest]
+        solution.append(next_vertex.i)
         points.pop(closest)
         j = j + 1
 
     return solution
+
+
+def simulated_annealing(points: list[Point], solution: list[int]) -> [list[int], float]:
+    """
+    get a solution (can be random or the results of the greedy algorithm, for a better baseline)
+    Then, apply simulated annealing algorithm on the given solution to improve the total cost (length of the road)
+
+    :param points:
+    :param solution: current working solution
+    :return: best working solution
+    """
+    number_of_iterations = 1000
+    initial_temperature = 100  # initial temperature
+    best_objective = calculate_length_of_tour(points, solution)  # evaluate the initial solution
+
+    for i in range(number_of_iterations):
+        temperature = initial_temperature / float(i + 1)  # calculate temperature for current epoch
+        start_index, end_index = sorted(random.sample(range(0, len(solution)), 2))
+        candidate_solution = \
+            solution[0:start_index] + list(reversed(solution[start_index:end_index])) + solution[end_index:]
+        # candidate_solution = solution[0:start_index] + solution[end_index:] + list(solution[start_index:end_index])
+        candidate_objective = calculate_length_of_tour(points, candidate_solution)
+
+        diff = candidate_objective - best_objective  # difference between candidate and current evaluations
+        metropolis = np.exp(-diff / temperature)  # calculate metropolis acceptance criterion
+
+        # check for new best solution or metropolis acceptance criterion satisfied
+        if (diff < 0) | (np.random.rand() < metropolis):
+            best_objective = candidate_objective
+            solution = candidate_solution  # store it
+
+    return solution, best_objective
 
 
 def solve_it(input_data):
@@ -86,7 +103,6 @@ def solve_it(input_data):
 
     # parse the input
     lines = input_data.split('\n')
-
     node_count = int(lines[0])
 
     points = []
@@ -96,19 +112,18 @@ def solve_it(input_data):
         parts = line.split()
         points.append(Point(float(parts[0]), float(parts[1]), i - 1))
 
-    # plot_points(points)
-    # solution, is_optimal = trivial(node_count), False
-    solution, is_optimal = greedy(points.copy()), False
-
-    # calculate the length of the tour
-    obj = length(points[solution[-1]], points[solution[0]])
-
-    for index in range(0, node_count - 1):
-        obj += length(points[solution[index]], points[solution[index + 1]])
+    # choose your desired algorithm to run:
+    # solution = trivial(node_count)
+    solution = greedy(points.copy())
+    solution, objective = simulated_annealing(points, solution)
+    is_optimal = False
 
     # prepare the solution in the specified output format
-    output_data = '%.2f' % obj + ' ' + str(int(is_optimal)) + '\n'
+    output_data = '%.2f' % objective + ' ' + str(int(is_optimal)) + '\n'
     output_data += ' '.join(map(str, solution))
+
+    # with open('solution.txt', 'w') as f:
+    #     f.write(output_data)
 
     return output_data
 
